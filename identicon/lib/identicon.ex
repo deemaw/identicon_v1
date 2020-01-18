29d83bbb28a5e0242b720bff304629d845098d2a
deema 99 <deemawattegdara@gmail.com>
2020-01-18 20:52:30 +0530

    we

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,11 +1,22 @@
 defmodule Identicon do
   def main( input ) do
     input
-    |> hash_input
-    |> pick_color
-    |> build_grid
-    |> filter_odd_squares
+      |> hash_input
+      |> pick_color
+      |> build_grid
+      |> filter_odd_squares
+      |> build_pixel_map
 
+  end
+  def build_pixel_map(%Identicon.Image{grid: grid}= image )  do
+    Enum.map grid , fn ( {_code, index} ) ->
+    horizontal = rem(index,5)*50
+    vertical = div(index,5)*50
+    top_left = {horizontal,vertical}
+    bottom_right = {horizontal +50 , vertical +50}
+    {top_left,bottom_right}
+    end
+    %Identicon.Image{image | pixel_map: pixel_map }
   end
 
   def filter_odd_squares( %Identicon.Image{grid: grid} = image ) do
